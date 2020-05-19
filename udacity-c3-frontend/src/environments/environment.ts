f4fce83681c08bd2b17c3ce6fa5a838c2329598e
tcoones <onur.ones@turkcell.com.tr>
2020-05-20 00:30:20 +0300

    added docker, kubernetes and travis congigurations

--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://192.168.99.100:8080/api/v0'
 };
 //apiHost: 'http://localhost:8080/api/v0'
 //apiHost: 'http://192.168.99.100:8080/api/v0'
